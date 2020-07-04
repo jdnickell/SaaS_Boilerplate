dc1e74d6f65931eb0e7eb39e3b171e3ed8485c9d
JD Nickell <jdnickell@clubessential.com>
2020-07-04 15:46:02 -0500

    add data models

---
+++ DataAccess/ChangeTrackingBaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DataAccess
+{
+    public class ChangeTrackingBaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime? CreateDateTime { get; set; }
+        public DateTime? UpdateDateTime { get; set; }
+        public int CreateById { get; set; }
+        public int UpdateById { get; set; }
+    }
+}
